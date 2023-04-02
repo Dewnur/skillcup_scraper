a557855e2dc53dbe70c8136dddfbc01173f48ef5
denis-kosobanov <kosobanovden@mail.ru>
2023-04-03 04:31:41 +0500

    add module model.py

---
+++ models.py
@@ -0,0 +1,40 @@
+from datetime import datetime
+from typing import NamedTuple
+
+
+class Task(NamedTuple):
+    id: int
+    name: int
+    card_id: int
+
+
+class Card(NamedTuple):
+    id: int
+    name: str
+    deadline: str
+    deadline_datetime: int = None
+
+
+class PersonCard(NamedTuple):
+    person_id: int
+    card_id: int
+    total: int = None
+    total_string: str = ''
+    id: int = None
+
+
+class Person(NamedTuple):
+    id: int
+    name: str
+    rate: str
+    tg_url: str
+    tg_name: str
+
+
+class Comment(NamedTuple):
+    id: int
+    task_id: int
+    person_id: int
+    content: str
+    overdue: bool
+    create_datetime: datetime
