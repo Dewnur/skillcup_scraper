0bf27494a8fa22305bdacef7aa0735d9922a918d
denis-kosobanov <kosobanovden@mail.ru>
2023-04-21 23:24:42 +0500

    add new fields Card, Task, Person, Comment

--- models.py
+++ models.py
@@ -6,13 +6,16 @@ class Task(NamedTuple):
     id: int
     name: int
     card_id: int
+    sequence_number: int
 
 
 class Card(NamedTuple):
     name: str
-    deadline: str
+    deadline_date: str
+    deadline_time: str = ''
     id: int = None
     ts_deadline: float = None
+    sequence_number: int = 0
 
 
 class PersonCard(NamedTuple):
@@ -29,12 +32,14 @@ class Person(NamedTuple):
     rate: str = ''
     tg_url: str = ''
     tg_name: str = ''
+    subscriber_count: int = 0
 
 
 class Comment(NamedTuple):
     task_id: int
     person_id: int
-    content: str
-    overdue: bool
-    ts_create: float
+    sequence_number: int
+    content: str = ''
+    overdue: bool = False
+    ts_create: float = 0
     id: int = None
